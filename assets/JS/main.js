175a6c348b70032c70d4c580bda3b01cefa3cb5d
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-14 14:20:48 +0200

    LEV_1_Conventions

---
+++ assets/JS/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("test").style.color = "red";
+}
+
+let helloworld = 'Hello World'
+const pi = 3.14
+const burjdubai = '828m'
+let fullName = 'Jan Schmidt'
+const eifeltower = '324m'
+let camelcase = 'camelCase'
+kursStatus = Boolean(true)
+
+
